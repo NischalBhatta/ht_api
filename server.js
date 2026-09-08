bc4a5578af84af86f54f3889319bc29f64ee8efc
NischalBhatta <nischalpb@gmail.com>
2026-09-08 16:15:58 +1000

    build: produce React dist folder and integrate static file serving in backend for production deployment

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
+import dns from "dns";
+
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
 import express from "express";
 import cors from "cors";
 import { connectMongoDB } from "./src/config/dbConfig.js";
 // import cors from "cors";
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 const PORT = 8000;
 connectMongoDB();
 
+//Static serving
+import path from "path";
+const __dirname = path.resolve();
+
+//Serve static files
+app.use(express.static(path.join(__dirname, "dist")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
 import habitRouters from "./src/routers/habitRouters.js";
 import completionRouters from "./src/routers/completionRouters.js";
-
-app.use(express.json());
-app.use(cors());
 
 app.use("/api/v1/habits", habitRouters);
 app.use("/api/v1/completions", completionRouters);
--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
 
-const mongoURL = "mongodb://localhost:27017/online_ht";
+// const mongoURL = "mongodb://localhost:27017/online_ht";
+const mongoURL = process.env.MONGO_URL;
 
 export const connectMongoDB = async () => {
+  console.log(mongoURL);
   try {
     const conn = await mongoose.connect(mongoURL);
     conn && console.log("Database connected");
